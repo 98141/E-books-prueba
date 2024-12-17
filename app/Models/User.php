b416a2f477fe4f47ad7327542381713563cc044c
Armando Mora <armandomora14115@gmail.com>
2024-12-17 07:42:02 -0500

    Pruebas

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+    public function author():HasOne {
+        return $this->hasOne(Author::class);
+    }
+
+    public function customer():HasOne {
+        return $this->hasOne(Customer::class);
+    }
+
+    // public function books(){
+    //     return $this->morphedByMany(Book::class, 'userable');
+    // }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Editorial;
+use App\Models\Author;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(GenreSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(AuthorSeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(SaleSeeder::class);
+        // $this->call(UserableSeeder::class);
+        Editorial::factory(10)->create();
+        User::factory(50)->create();
+        Author::factory(50)->create();
+        User::factory(10)->create();
+        Author::factory(10)->create();
+    }
+}
